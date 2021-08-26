287186353a5b0fbcfe3cd825a4f8d90d4dd558bc
romilson ramos <41062660+roni96@users.noreply.github.com>
2021-08-26 10:54:13 -0400

    Add files via upload

---
+++ calculadora-IP.py
@@ -0,0 +1,27 @@
+#
+# Dev: Romilson Ramos     
+# Versão Beta ©
+#
+# Utiliza aa biblioteca ipaddress para calculo de IP
+# Utiliza aa biblioteca tqdm para exibição de barra de progresso.
+#
+
+import ipaddress
+from tqdm import tqdm
+
+#Abrir arquivo
+arq_resultado = open('arquivo.txt', 'r')
+#Criar lista
+resultado = arq_resultado.readlines()
+
+#Solicita a rede ou IP ao usuário.
+rede = input('Qual rede? ')
+#Estanciado o objeto network que vai receber a rede do IP fornecido anteriormente
+network = ipaddress.IPv4Network(rede) 
+
+for x in tqdm(network.hosts()):
+	resultado.append("{}\n".format(x))   # insira seu conteúdo
+
+arq_resultado = open('arquivo.txt', 'w') # Abre novamente o arquivo (escrita)
+arq_resultado.writelines(resultado)    # escreva o conteúdo criado anteriormente nele.
+arq_resultado.close()
